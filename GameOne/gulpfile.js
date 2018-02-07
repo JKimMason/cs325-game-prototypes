d05b042e8238e5f470315221a4ba20359f7c0d88
Jay Kim <jkim208@masonlive.gmu.edu>
2018-02-06 20:42:27 -0500

    gulpfile fixed

--- GameOne/gulpfile.js
+++ GameOne/gulpfile.js
@@ -2,15 +2,16 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function(){
   browserSync.init({
     server: {
-      baseDir: ['src']
+      baseDir: 'src/'
     },
   })
-  gulp.watch('main/*.html', browserSync.reload);
-  gulp.watch('css/style.css', browserSync.reload);
-  gulp.watch('js/*.js', browserSync.reload);
+
+  gulp.watch('src/*.html', browserSync.reload);
+  gulp.watch('src/css/style.css', browserSync.reload);
+  gulp.watch('src/js/*.js', browserSync.reload);
 })
 
 // Default task
